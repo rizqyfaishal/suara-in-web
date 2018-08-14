8165e101b1dd6a613a2614755da0a3462f11f05b
Rizqy Faishal <rizqyfaishal27@gmail.com>
2018-08-14 23:36:27 +0700

    update production server launch

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,7 +2,7 @@ const path = require('path');
 const compression = require('compression');
 const express = require('express');
 
-moduele.exports = function createProdConfig(app) {
+module.exports = function createProdConfig(app) {
 	const buildPath = path.resolve(process.cwd(), 'dist');
 
 	app.use(compression());
@@ -10,4 +10,6 @@ moduele.exports = function createProdConfig(app) {
 	app.get('*', function(req, res) {
 		return res.sendFile(path.resolve(buildPath, 'index.html'));
 	})
+
+  return app;
 }
